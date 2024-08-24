cb9bd2bf10987d5dbc7ee521d3f6a4b6c82591ba
Janardhan-Majumder <janardhan.md03@gmail.com>
2024-08-24 18:19:16 +0600

    business list page lightly touch

--- src/app/(commonLayout)/business/page.tsx
+++ src/app/(commonLayout)/business/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import BusinessCart from "@/components/BusinessCart";
 import { useGetBunsinessQuery } from "@/redux/features/business/businessApi";
 import React, { useEffect, useState } from "react";
 
@@ -7,43 +8,52 @@ export default function BusinessList({
 }: {
   searchParams: { service: string; suburb: string; postcode: string };
 }) {
+  const [page, setPage] = useState<number | null>(1);
   const [business, setBusiness] = useState<{ [key: string]: any }[]>([]);
-  const { data, isLoading, isError } = useGetBunsinessQuery([
-    { name: "serviceId", value: searchParams.service },
-    { name: "postalCode", value: searchParams.postcode },
-    { name: "suburb", value: searchParams.suburb },
-  ]);
-  useEffect(() => {
-    setBusiness(c=> ([...c, data?.data]))
-  }, [data?.data]);
-
   if (!searchParams.service || !searchParams.postcode || !searchParams.suburb)
     return (
       <h2 className="text-2xl font-medium text-center">No services found</h2>
     );
-
-  //   if (providers.length === 0)
-  //     return (
-  //       <h2 className="text-2xl font-medium text-center">No result found</h2>
-  //     );
-
+  const { data, isLoading, isError } = useGetBunsinessQuery([
+    { name: "serviceId", value: searchParams.service },
+    { name: "postalCode", value: searchParams.postcode },
+    { name: "suburb", value: searchParams.suburb },
+    { name: "page", value: page },
+  ]);
+  useEffect(() => {
+    if (data?.data) {
+      setBusiness((c) => [...c, data?.data]);
+      setPage(data.pagination.nextPage);
+    }
+  }, [data?.data]);
   return (
     <section className="flex flex-col gap-8 lg:px-36 lg:py-16 px-2 py-4">
-      {/* {providers.map((service) => (
-        <BusinessCart
-          key={service._id.toString()}
-          data={{
-            id: service.userId._id.toString(),
-            description: service.cleanAbout,
-            image: service.userId.image,
-            name: service.businessName,
-            email: service.userId.email,
-            mobile: service.userId.mobile,
-            providerId: service._id.toString(),
-          }}
-        />
-      ))} */}
-      fdklfjdslk
+      {business.map((busines, indx) => (
+        <BusinessCart key={indx} data={busines} />
+      ))}
+      {page && (
+        <button
+          // onClick={business.}
+          className="flex items-center gap-2 rounded-t-[3px] border-[.5px] border-b-2 py-3 px-5 border-white bg-green-600 text-white  mx-auto"
+        >
+          See more{" "}
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3.75 16.25L16.25 3.75M16.25 3.75H6.875M16.25 3.75V13.125"
+              stroke="#FFFFFF"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      )}
     </section>
   );
 }
--- src/components/BusinessCart.tsx
+++ src/components/BusinessCart.tsx
@@ -2,44 +2,34 @@ import Image from "next/image";
 import Link from "next/link";
 import BusinessContact from "./business/BusinessContact";
 
-function BusinessCart({
-  data: { image, name, description, id, email, mobile, providerId },
-}: {
-  data: {
-    image: string;
-    name: string;
-    description: string;
-    id: string;
-    providerId: string;
-    email: string;
-    mobile: string;
-  };
-}) {
+function BusinessCart({ data }: { data: { [key: string]: any } }) {
+  console.log(data)
   return (
     <div className="p-6 flex flex-col lg:flex-row items-center lg:items-start  gap-4 rounded-xl border-2 border-green-500 hover:bg-green-50 transition-all">
-      <Link href={"/business/" + id}>
-        <Image
+      <Link href={"/business/" + data.id}>
+        {/* <Image
           src={image}
           alt={name}
           className="rounded-lg w-[150px] h-[150px] content-evenly"
           width={150}
           height={150}
           priority
-        />
+        /> */}
+        dfdsklfjsd
       </Link>
       <div>
         <Link
-          href={"/business/" + id}
+          href={"/business/" + data.id}
           className="text-2xl font-medium hover:text-green-500"
         >
-          {name}
+          {data.name}
         </Link>
-        <p className="my-4">
+        {/* <p className="my-4">
           {description.length > 360
             ? description.slice(0, 360).concat("...")
             : description}
-        </p>
-        <BusinessContact email={email} number={mobile} />
+        </p> */}
+        <BusinessContact email={data?.user?.email} number={data.mobile} />
       </div>
     </div>
   );
