928829152309191c239d9571d1ea0c0f77302f15
Janardhan-Majumder <janardhan.md03@gmail.com>
2024-10-28 15:52:33 +0600

    job list api correction

--- src/components/Jobs/JobList.tsx
+++ src/components/Jobs/JobList.tsx
@@ -4,28 +4,34 @@ import React, { useState } from "react";
 import LoaderWraperComp from "../LoaderWraperComp";
 import Pagination from "../Pagination";
 import JobCard from "./JobCard";
+import { useAppSelector } from "@/redux/hooks";
+import { selectCurrentUser } from "@/redux/features/users/authSlice";
 
 const JobList = () => {
+  const { user } = useAppSelector(selectCurrentUser);
   const [currentPage, setCurrentPage] = useState(1);
   // const router = useRouter()
-  const { data, isLoading, isError } = useGetjobsQuery([
+  const { data, isLoading, isError } = useGetjobsQuery(
+    [
+      {
+        name: "limit",
+        value: 15,
+      },
+      {
+        name: "page",
+        value: currentPage,
+      },
+      {
+        name: "businessId",
+        value: user?.business?.id || "",
+      },
+    ],
     {
-      name: "limit",
-      value: 15,
-    },
-    {
-      name: "page",
-      value: currentPage,
-    },
-    // {
-    //   name: "businessId",
-    //   value: "",
-    // },
-  ]);
-  // console.log(router.pathname);
-
+      skip: !user,
+    }
+  );
   return (
-    <LoaderWraperComp isLoading={isLoading} isError={isError} height="h-[80vh]">
+    <LoaderWraperComp isLoading={isLoading} isError={isError} height="h-[80vh]" dataEmpty={data?.data?.length < 1}>
       <div className="px-[32px] py-[32px] space-y-6">
         {data?.data?.map((job: any, index: number) => (
           <JobCard job={job} key={index} />
